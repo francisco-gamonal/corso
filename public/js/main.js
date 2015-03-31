efe1dfa5e6b5a7ccf718fa47f98963a2643d3faf
Francisco Gamonal <hfgamonalb@gmail.com>
2015-03-31 10:11:08 -0500

    range dates view product

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,70 @@
+var server = "";
+var pathname = document.location.pathname;
+var pathnameArray= pathname.split("/public/");
+
+server =  pathnameArray.length > 0 ? pathnameArray[0]+"/public/" : "";
+
+//Function Ajax
+var ajaxForm = function (url, type, data){
+	var message;
+	var path = url;
+	if(type == 'post'){
+		message = 'Registrando';
+	}else{
+		message = 'Actualizando';
+	}
+	return $.ajax({
+				url: path,
+			    type: type,
+			    data: {data: JSON.stringify(data)},
+			    datatype: 'json',
+			    beforeSend: function(){
+		    		//loadingUI(message);
+		    		console.log("antes de ir");
+			    },
+			    error:function(){
+			    	//$.unblockUI();
+			    	//bootbox.alert("<p class='red'>No se pueden grabar los datos.</p>")
+			    	//responseUI('No se pueden grabar los datos.', 'red');
+			    	console.log("No se pueden grabar los datos");
+			    }
+			});
+};
+
 $(function(){
-	console.log("OK");
+	var data = {};
+	$("#txtDate").daterangepicker(
+		{
+			locale:
+			{
+				monthNames: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Set','Oct','Nov','Dic'],
+				applyLabel: 'Aceptar',
+				cancelLabel: 'Cancelar',
+				fromLabel: 'Desde',
+				toLabel: 'Hasta'
+			},
+			minViewMode: 'month',
+		    format: 'MM/YYYY',
+		    startDate: $('#startDate').val(),
+		    endDate: $('#endDate').val(),
+		    minDate: $('#startDate').val(),
+		    maxDate: $('#endDate').val(),
+		    hideFormInputs: false,
+		    opens: 'right',
+		    autoApplyClickedRange : true
+		},
+		function(start, end, label) {
+		 	$('#txtDate').val(start.format('MM/YYYY')+'-'+end.format('MM/YYYY'));
+		}
+	);
+
+	$(document).on('change', '#txtDate', function(){
+		var range  = $('#txtDate').val();
+		data.range = range;
+		console.log(data, pathname);
+		ajaxForm(pathname, 'post', data)
+		.done( function (data){
+			console.log("OK");
+		});
+	});
 });
--- resources/views/claro/product.blade.php
+++ resources/views/claro/product.blade.php
@@ -1,29 +1,33 @@
 @extends('template.main')
 
+@section('styles')
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/lib/daterangepicker-bs3.css') }}">
+@endsection
+
 @section('content')
 	@if(!$producto->isEmpty())
-	<div class="product">
-		<form class="navbar-form form-inline" role="form">
-			<input type="hidden" id="startDate" value="1/2014">
-			<input type="hidden" id="endDate" value="1/2015">
-			<input type="hidden" id="year" value="">
-			<input type="hidden" id="month" value="" page="report">
-			<input type="hidden" id="eyear" value="">
-			<input type="hidden" id="emonth" value="">
-			<label class="col-sm-1" style="line-height:2.5;text-align:right;">Fecha</label>
-			<div id="txtDate" class="form-group col-sm-3">
+		@foreach($producto as $product)
+		<div class="product">
+			<h2 class="text-center">Claro - {{$product->name}}</h2>
+			<label class="col-sm-2" style="line-height:2.5;text-align:right;">Rango de Fecha</label>
+			<div class="form-group col-sm-3">
 				<div class="input-group rdate">
-					<input type="text" class="form-control"><span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>
+					<input id="startDate" type="hidden" value="{{ str_pad($inicioRecord->mes, 2, '0', STR_PAD_LEFT).'/'.$inicioRecord->year }}">
+					<input id="endDate" type="hidden" value="{{ str_pad($finalRecord->mes, 2, '0', STR_PAD_LEFT).'/'.$finalRecord->year }}">
+					<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span><input id="txtDate" type="text" class="form-control">
 				</div>
 			</div>
+		</div>
+		<div class="data_product">
 			
-			
-		</form>		
-	</div>
-	<div class="data_product">
-		
-	</div>
+		</div>
+		@endforeach
 	@else
 		No existe
 	@endif
+@endsection
+
+@section('scripts')
+	<script type="text/javascript" src="{{ asset('js/lib/moment.js')}}"></script>
+	<script type="text/javascript" src="{{ asset('js/lib/daterangepicker.js') }}"></script>
 @endsection

c4beac25a0393c7800fa26fee3645c0d6820bf97
Anwar Sarmiento <anwarsarmiento@gmail.com>
2015-03-30 21:01:10 -0600

    empleados y routes

--- app/Http/Controllers/ClaroController.php
+++ app/Http/Controllers/ClaroController.php
@@ -1,84 +1,213 @@
-<?php namespace Corso\Http\Controllers;
+<?php
 
-use Corso\Http\Requests;
-use Corso\Http\Controllers\Controller;
+namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ClaroController extends Controller {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-            return View::make('claro.index');
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        return View::make('claro.index');
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create() {
+        //
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store() {
+        //
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id) {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id) {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id) {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id) {
+        //
+    }
+
+    /**
+     * 
+     * @param type $id
+     * @return type
+     */
+    public function importarClaro($id) {
+
+        $data = Empresa::find($id);
+        $claro = $data->Productos()->lists('name', 'id');
+        array_unshift($claro, ' --- Seleccione un Prodcuto --- ');
+        $mes = $this->Mes();
+        return View::make('claro.importar', compact('claro', 'mes'));
+    }
+    /**
+     * 
+     * @return type
+     */
+    public function dataProduct() {
+
+        $empresa = Empresa::find(1);
+        return View::make('claro.productos', compact('empresa'));
+    }
+
+    /**
+     * Mostramos todos los datos en la vista de la tabla Datos empresas de claro
+     * @return type
+     */
+    public function ListaDatosEmpresas() {
+        $datosEmpresas = DatosEmpresa::paginate(100);
+        return View::make('claro.listaDatosEmpresas', compact('datosEmpresas'));
+    }
+    
+    /**
+     * aqui ejecutamos todos los metodos para agregar un nuevo archivo o reemplazarlo
+     * @return type
+     */
+    public function importarExcelClaro() {
+        set_time_limit(0);
+        ini_set('memory_limit', '20240M');
+        /* de claramos las variables que recibimos por post */
+        $mes = Input::get('mes');
+        $year = Input::get('year');
+        $producto = Input::get('productos_id');
+        $file = Input::file('excel');
+        $url = "files/claro/CICLO" . $producto . str_pad($mes, 2, '0', STR_PAD_LEFT) . $year . ".xlsx";
+
+
+        /* agregamos un nuevo historial y retornamos el ID o buscamos regresamos el ID */
+        $idHistorial = HistorialsController::SaveHistorials($mes, $year, $producto, $url);
+
+        /* Corremos el archivo de excel y lo convertimos en un array */
+        $excel = EmpresasController::uploadExcel($file, 'claro', 'CICLO' . $producto . str_pad($mes, 2, '0', STR_PAD_LEFT) . $year . '.xlsx');
+
+        return $this->saveExcel($excel, $idHistorial);
+    }
+    /**
+     * 
+     * @param type $data
+     * @param type $historial
+     * @return type
+     */
+    private function saveExcel($data, $historial) {
+
+        $datos = DatosEmpresa::where('historials_id', '=', $historial)->delete();
+
+        foreach ($data AS $dataExcel):
+            $datos_empresas = new DatosEmpresa;
+            $datos_empresas->barra = null;
+            if (empty($dataExcel['codigo'])):
+                $datos_empresas->codigo = null;
+            else:
+                $datos_empresas->codigo = $dataExcel['codigo'];
+            endif;
+            if (empty($dataExcel['tipo_cliente'])):
+                $datos_empresas->tipo_cliente = null;
+            else:
+                $datos_empresas->tipo_cliente = $dataExcel['tipo_cliente'];
+            endif;
+            if (empty($dataExcel['telefono'])):
+                $datos_empresas->telefono = null;
+            else:
+                $datos_empresas->telefono = $dataExcel['telefono'];
+            endif;
+            if (empty($dataExcel['nombre_cliente'])):
+                $datos_empresas->name_cliente = null;
+            else:
+                $datos_empresas->name_cliente = $dataExcel['nombre_cliente'];
+            endif;
+            if (empty($dataExcel['comentario'])):
+                $datos_empresas->comentario = null;
+            else:
+                $datos_empresas->comentario = $dataExcel['comentario'];
+            endif;
+            if (empty($dataExcel['fecha_entrega'])):
+                $datos_empresas->fecha_entregado = null;
+            else:
+                $datos_empresas->fecha_entregado = $dataExcel['fecha_entrega'];
+            endif;
+            if (empty($dataExcel['fecha_recibido'])):
+                $datos_empresas->fecha_recibido = null;
+            else:
+                $datos_empresas->fecha_recibido = $dataExcel['fecha_recibido'];
+            endif;
+            if (empty($dataExcel['monto'])):
+                $datos_empresas->monto = null;
+            else:
+                $datos_empresas->monto = $dataExcel['monto'];
+            endif;
+            if (empty($dataExcel['direccion'])):
+                $datos_empresas->direccion = null;
+            else:
+                $datos_empresas->direccion = $dataExcel['direccion'];
+            endif;
+            if (empty($dataExcel['comentario_ciudad'])):
+                $datos_empresas->comentario_ciudad = null;
+            else:
+                $datos_empresas->comentario_ciudad = $dataExcel['comentario_ciudad'];
+            endif;
+            $datos_empresas->ciudades_id = Ciudade::convertionCiudad($dataExcel['ciudad']);
+            if (empty($dataExcel['observaciones'])):
+                $datos_empresas->observaciones_id = 16;
+            else:
+                $datos_empresas->observaciones_id = $dataExcel['observaciones'];
+            endif;
+            $datos_empresas->historials_id = $historial;
+            if (empty($dataExcel['empleados'])):
+                $datos_empresas->empleados_id = null;
+            else:
+                $datos_empresas->empleados_id = $dataExcel['empleados'];
+            endif;
+            $datos_empresas->save();
+        endforeach;
+        return Redirect::to('claro/ciclo')->with('messege', 'se guardo con exito!!');
+    }
 
 }
--- app/Http/Controllers/EmpleadosController.php
+++ app/Http/Controllers/EmpleadosController.php
@@ -1,84 +1,99 @@
-<?php namespace Corso\Http\Controllers;
+<?php
+
+namespace Corso\Http\Controllers;
 
 use Corso\Http\Requests;
 use Corso\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
 
 class EmpleadosController extends Controller {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        $empleados = Empleado::paginate(15);
+        return View::make('empleados.index', compact('empleados'));
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create() {
+        $ciudades = Ciudade::lists('name', 'id');
+        return View::make('empleados.create', compact('ciudades'));
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store() {
+        $empleados = Input::all();
+        $empleado = new Empleado($empleados);
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+        /* Validamos los datos para guardar tabla menu */
+        if ($empleado->isValid((array) $empleados)):
+            $empleado->fill($empleados);
+            $empleado->save();
+            /* Enviamos el mensaje de guardado correctamente */
+            return Redirect::route('ver-empleados')->with(array('message' => 'Los datos se guardaron con exito!!!'));
+        endif;
+        /* Enviamos el mensaje de error */
+        return Redirect::route('registrar-empleados')->withInput()->withErrors($empleado->errors);
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id) {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id) {
+        $empleado = Empleado::find($id);
+        $ciudades = Ciudade::lists('name', 'id');
+        return View::make('empleados.edit', compact('empleado', 'ciudades'));
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id) {
+        $empleados = Input::all();
+        $empleado = Empleado::find($empleados['id']);
+        /* Validamos los datos para guardar tabla menu */
+        $empleado->fill($empleados);
+        $empleado->save();
+        /* Enviamos el mensaje de guardado correctamente */
+        return Redirect::route('ver-empleados')->with(array('message' => 'Los datos se guardaron con exito!!!'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id) {
+        //
+    }
 
 }

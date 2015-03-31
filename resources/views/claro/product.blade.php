437df44fbc18937ff44f7641c7e1189ad3aac6bb
Francisco Gamonal <hfgamonalb@gmail.com>
2015-03-31 15:52:56 -0500

    range dates view product

--- resources/views/claro/product.blade.php
+++ resources/views/claro/product.blade.php
@@ -9,12 +9,14 @@
 		@foreach($producto as $product)
 		<div class="product">
 			<h2 class="text-center">Claro - {{$product->name}}</h2>
-			<label class="col-sm-2" style="line-height:2.5;text-align:right;">Rango de Fecha</label>
-			<div class="form-group col-sm-3">
-				<div class="input-group rdate">
-					<input id="startDate" type="hidden" value="{{ str_pad($inicioRecord->mes, 2, '0', STR_PAD_LEFT).'/'.$inicioRecord->year }}">
-					<input id="endDate" type="hidden" value="{{ str_pad($finalRecord->mes, 2, '0', STR_PAD_LEFT).'/'.$finalRecord->year }}">
-					<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span><input id="txtDate" type="text" class="form-control">
+			<div class="text-center">
+				<label class="text-center">Rango de Fechas</label>
+				<div class="form-group">
+					<div class="input-group col-sm-4 col-sm-offset-4">
+						<input id="startDate" type="hidden" value="{{ str_pad($inicioRecord->mes, 2, '0', STR_PAD_LEFT).'/'.$inicioRecord->year }}">
+						<input id="endDate" type="hidden" value="{{ str_pad($finalRecord->mes, 2, '0', STR_PAD_LEFT).'/'.$finalRecord->year }}">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span><input id="txtDate" type="text" class="form-control">
+					</div>
 				</div>
 			</div>
 		</div>

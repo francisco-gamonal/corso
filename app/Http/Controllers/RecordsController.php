88c203e5f4413a91ab934dc3a3e23615ab6a8938
Anwar Sarmiento <anwarsarmiento@gmail.com>
2015-04-27 03:16:45 -0600

    Pdf Prueba

--- app/Http/Controllers/RecordsController.php
+++ app/Http/Controllers/RecordsController.php
@@ -22,149 +22,253 @@ class RecordsController extends Controller {
      * @return Response
      */
     public function historialProductos($name) {
+
         /** Enviamos a buscar la empresa a consultar por el nombre recuperamos
+
          * el Id para consulta en productos
+
          * */
         $business = Business::where('name', $name)->get();
+
         /** consultamos con el ID de la empresa los productos que le pretenecen
+
          * y recuperamos los IDs de los productos de la empresa
+
          */
         $products = Product::where('empresas_id', $business[0]->id)->get();
+
         /** Recorremos los productos para hacer consulta de cada uno en el historial
+
          * para obtener cada uno de los registros de cada productos
+
          */
         foreach ($products AS $product):
+
             /** buscamos con el ID de los productos los instortiales para enviarlos 
+
              * como arreglos a la vista para mostras cada historial registrado
+
              */
             $record[] = Record::where('productos_id', $product->id)->get();
+
         endforeach;
+
         $records = $record;
+
         return View('record.index', compact('records'));
     }
 
     /**
+
      * Show the form for creating a new resource.
+
      *
+
      * @return Response
+
      */
     public function create() {
+
         //
     }
 
     /**
+
      * Store a newly created resource in storage.
+
      *
+
      * @return Response
+
      */
     public function store() {
+
         //
     }
 
     /**
+
      * Display the specified resource.
+
      *
+
      * @param  int  $id
+
      * @return Response
+
      */
     public function show($id) {
+
         //
     }
 
     /**
+
      * Show the form for editing the specified resource.
+
      *
+
      * @param  int  $id
+
      * @return Response
+
      */
     public function edit($id) {
+
         //
     }
 
     /**
+
      * Update the specified resource in storage.
+
      *
+
      * @param  int  $id
+
      * @return Response
+
      */
     public function update($id) {
+
         //
     }
 
     /**
+
      * Remove the specified resource from storage.
+
      *
+
      * @param  int  $id
+
      * @return Response
+
      */
     public function destroy($id) {
+
         //
     }
 
     public static function recordSeparator($id) {
 
+
+
         $Record = Record::find($id);
+
         $Record = explode(' ', strtolower($Record->products->name));
+
         switch (count($Record)):
+
             case 1:
+
                 $Record = $Record[0];
+
                 break;
+
             case 2:
+
                 $Record = $Record[0] . '-' . $Record[1];
+
                 break;
+
             case 3:
+
                 $Record = $Record[0] . '-' . $Record[1] . '-' . $Record[2];
+
                 break;
+
             case 4:
+
                 $Record = $Record[0] . '-' . $Record[1] . '-' . $Record[2] . '-' . $Record[3];
+
                 break;
+
         endswitch;
+
+
 
         return $Record;
     }
 
     /**
+
      * @author Anwar Sarmiento
+
      * @param type $mes
+
      * @param type $year
+
      * @param type $producto
+
      * @return type
+
      * @description  Guardamos el historial de los productos 
+
      */
     public static function SaveHistorials($mes, $year, $producto, $url) {
+
         /* Buscamos ver si ya existe el archivo que se quiere agregar */
+
         $verificacion = Record::where('mes', '=', $mes)
                         ->where('year', '=', $year)
                         ->where('productos_id', '=', $producto)->get();
 
+
+
         /* si existe enviamos el id */
+
         if ($verificacion->isEmpty() == false):
+
+
 
             return $verificacion[0]->id;
 
+
+
         endif;
+
         /*  de lo contrario agregamos el nuevo historial */
+
         $Historial = new Record;
+
         $Historial->mes = $mes;
+
         $Historial->year = $year;
+
         $Historial->productos_id = $producto;
+
         $Historial->url = $url;
+
         $Historial->save();
 
+
+
         /* Retornamos el id de la nueva fila */
+
         $id = Record::all()->last();
+
         return $id->id;
     }
 
     public function descargasProducto($id) {
 
+
+
         set_time_limit(0);
+
         ini_set('memory_limit', '20048M');
+
         $historial = Record::find($id);
+
         $dataCompanie = DataCompanie::where('historials_id', '=', $id)->get();
+
         $separar = explode('/', $historial->url);
+
         $quitarExtencion = explode('.', $separar[2]);
+
         $data = array();
+
         $data[] = array('Codigo',
             'Tipo Cliente',
             'Telefono',
@@ -178,7 +282,10 @@ class RecordsController extends Controller {
             'monto',
             'direccion',
             'comentario ciudad', 'empleados');
+
         foreach ($dataCompanie as $value):
+
+
 
             $data[] = array($value->codigo,
                 $value->tipo_cliente,
@@ -195,59 +302,122 @@ class RecordsController extends Controller {
                 $value->comentario_ciudad,
                 $value->empleados_id
             );
-
-//
         endforeach;
-
         Excel::create($quitarExtencion[0], function($excel) use ($data) {
-
             $excel->sheet('Datos Descargados', function($sheet) use ($data) {
+
                 $sheet->fromArray($data, null, 'A1', false, false);
             });
         })->download('xlsx');
     }
 
     /**
+
      * Con este metodo generamos un archivo de PDF
-     * para que puedan ver la información trabajada los clientes.
+
+     * para que puedan ver la informaciÃ³n trabajada los clientes.
+
      * @param type $id
+
      * @return type
+
      */
     public function pdfClientes($id) {
-        set_time_limit(0);
-        ini_set('memory_limit', '20048M');
+
+      
+
         $dataCompanies = DataCompanie::where('historials_id', $id)->get();
-        $pdf = \PDF::loadView('claro.reportPdf', compact('dataCompanies'));
-     //   $dataCompanies = DataCompanie::where('historials_id', $id)->get();
-     //   $pdf = App::make('dompdf');
-     //   $pdf->loadView('claro.reportPdf');
-        return $pdf->setOrientation('landscape')->stream();
+
+        $html = "<html><body>
+        <h2 style='margin-top:-1em; text-align:center;'>Reporte de Entregas</h2>
+        <table id='table_dataProduct' class='table table-striped table-bordered table-hover' cellspacing='0' width='100%'>
+            <thead>
+               <tr style='font-size: 14px'>
+                    <th>Código</th>
+                    <th>Tipo Cliente</th>
+                    <th>Nombre Cliente</th>
+                    <th>Ciudad</th>
+                    <th>Estado</th>
+                    <th>Observación</th>
+                    <th>Comentario</th>
+                    <th>Empleados</th>
+                </tr>
+            </thead>
+            <tbody>";
+        foreach ($dataCompanies as $dataCompanie):
+            $html .="<tr>
+                    <td>$dataCompanie->codigo</td>
+                    <td>$dataCompanie->tipo_cliente</td>
+                    <td>$dataCompanie->name_cliente</td>";
+            if (empty($dataCompanie->ciudades_id)):
+                $html .="<td></td>";
+            else:
+                $html .="<td>$dataCompanie->ciudades_id</td>";
+            endif;
+            if (empty($dataCompanie->observaciones_id)):
+                $html .="<td></td>
+                    <td></td>";
+            else:
+                $html .="<td>$dataCompanie->observations->status->name</td>
+                    <td>$dataCompanie->observations->name</td>";
+            endif;
+            $html .="<td>$dataCompanie->comentario</td>";
+            if (empty($dataCompanie->empleados_id)):
+                $html .="<td></td>";
+            else:
+                $html .="<td>$dataCompanie->empleados_id</td>";
+            endif;
+            $html .="</tr>";
+        endforeach;
+        $html .="</tbody>
+        </table>
+    </body></html>";
+        $pdf = App::make('dompdf'); //Note: in 0.6.x this will be 'dompdf.wrapper'
+        $pdf->loadHTML($html);
+        return $pdf->stream();
     }
 
     /**
+
      * Con este metodo generamos un archivo de Excel
-     * para que puedan ver la información trabajada los clientes.
+
+     * para que puedan ver la informaciÃ³n trabajada los clientes.
+
      * @param type $id
+
      */
     public function descargasProductoClientes($id) {
 
+
+
         set_time_limit(0);
+
         ini_set('memory_limit', '20048M');
+
         $historial = Record::find($id);
+
         $dataCompanie = DataCompanie::where('historials_id', $id)->get();
+
         $count = DataCompanie::where('historials_id', $id)->count();
+
         $separar = explode('/', $historial->url);
+
         $quitarExtencion = explode('.', $separar[2]);
-        $data[] = array('N°', 'Codigo',
+
+        $data[] = array('NÂ°', 'Codigo',
             'Nombre Cliente',
             'Tipo Cliente',
             'Estado',
             'Observaciones',
             'Comentario', 'Ciudad', 'Empleados'
         );
+
         $i = 0;
+
         foreach ($dataCompanie as $value):
+
             $i++;
+
             $data[] = array($i, $value->codigo,
                 $value->tipo_cliente,
                 $value->name_cliente,
@@ -259,47 +429,85 @@ class RecordsController extends Controller {
             );
 
 
+
+
+
         endforeach;
+
         $name = $value->records->products->business->name;
+
         $producto = $value->records->products->name;
+
         $mes = $value->records->mes;
+
         $year = $value->records->year;
+
         $count = $count + 2;
+
         Excel::create($name . ' ' . $mes . '-' . $year . ' ' . $producto, function($excel) use ($data, $count, $name, $producto, $mes, $year) {
+
             // Set the title
+
             $excel->setTitle('Reporte de Datos consultados');
 
+
+
             // Chain the setters
+
             $excel->setCreator('El Corso - Sistemas Amigables - Costa Rica')
                     ->setCompany('El Corso');
 
+
+
             // Call them separately
-            $excel->setDescription('La información generada es de uso exclusivo de ' . $name);
+
+            $excel->setDescription('La informaciÃ³n generada es de uso exclusivo de ' . $name);
+
+
 
             $excel->sheet($mes . '-' . $year . ' ' . $producto, function($sheet) use ($data, $count) {
+
                 $sheet->mergeCells('A1:I1');
+
                 $sheet->setAutoSize(true);
+
                 $sheet->setFontBold(true);
+
                 $sheet->setBorder('A1:I' . $count, 'thin');
+
                 $sheet->cells('A1:I1', function($cells) {
 
+
+
                     // Set alignment to center
+
                     $cells->setAlignment('center');
+
                     // Set font
+
                     $cells->setBackground('#df0000');
+
                     // Set with font color
+
                     $cells->setFontColor('#ffffff');
+
                     $cells->setFont(array(
                         'family' => 'Calibri',
                         'size' => '16',
                         'bold' => true,
                     ));
                 });
+
                 $sheet->cells('A2:I2', function($cells) {
+
                     // Set font
+
                     $cells->setBackground('#df0000');
+
                     // Set with font color
+
                     $cells->setFontColor('#ffffff');
+
                     $cells->setFont(array(
                         'family' => 'Calibri',
                         'size' => '12',
@@ -307,8 +515,12 @@ class RecordsController extends Controller {
                     ));
                 });
 
+
+
                 $sheet->row(1, array('Reporte de Entregas'));
+
                 $sheet->fromArray($data, null, 'A2', false, false);
+
                 //$sheet->row(1,$data);
             });
         })->export('xlsx');

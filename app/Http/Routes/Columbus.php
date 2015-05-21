ecedb281ebd98fba53f4fa73151ca57225fc1a25
Francisco Gamonal <hfgamonalb@gmail.com>
2015-05-21 17:20:05 -0500

    commment product name

--- app/Http/Routes/Columbus.php
+++ app/Http/Routes/Columbus.php
@@ -4,7 +4,7 @@
  * Claro
  */
 Route::get('columbus/estado-de-cuenta', ['as' => 'columbus', 'uses' => 'ColumbusController@index']);
-Route::get('columbus/{name}', ['as' => 'producto_claro', 'uses' => 'ProductsController@getProduct']);
+//Route::get('columbus/{name}', ['as' => 'producto_claro', 'uses' => 'ProductsController@getProduct']);
 Route::post('columbus/search', ['as' => 'data_product', 'uses' => 'ColumbusController@dataProduct']);
 Route::get('columbus/importar-ciclo/{id}', ['as' => 'importar-ciclo', 'uses' => 'ColumbusController@importarClaro']);
 Route::post('columbus/importar-ciclo', ['as' => 'save-ciclo', 'uses' => 'ColumbusController@importarExcelClaro']);
